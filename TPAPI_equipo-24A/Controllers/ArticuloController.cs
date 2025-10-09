1bddc4f5bc191f86933836f8313bdba0fd00e38d
PameKid <pamelat.vizcarra@gmail.com>
2025-10-08 23:42:52 -0300

    Agregar con DTO

--- TPAPI_equipo-24A/Controllers/ArticuloController.cs
+++ TPAPI_equipo-24A/Controllers/ArticuloController.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Dominio;
 using Negocio;
+using TPAPI_equipo_24A.Models;
 
 
 namespace TPAPI_equipo_24A.Controllers
@@ -29,8 +30,19 @@ namespace TPAPI_equipo_24A.Controllers
         }
 
         // POST: api/Articulo
-        public void Post([FromBody]string value)
+        public void Post([FromBody]ArticuloDTO art)
         {
+            ArticuloNegocio aNegocio = new ArticuloNegocio();
+            Articulo articuloNuevo = new Articulo();
+            articuloNuevo.CodArticulo = art.CodArticulo;
+            articuloNuevo.NombreArticulo = art.NombreArticulo;
+            articuloNuevo.Marca = new Marca { IdMarca = art.IdMarca };
+            articuloNuevo.Categoria = new Categoria { IdCategoria = art.IdCategoria };
+            articuloNuevo.Descripcion = art.Descripcion;
+            articuloNuevo.Precio = art.Precio;
+            
+            aNegocio.agregar(articuloNuevo);
+
         }
 
         // PUT: api/Articulo/5
---
+++ TPAPI_equipo-24A/Models/ArticuloDTO.cs
@@ -0,0 +1,22 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace TPAPI_equipo_24A.Models
+{
+    public class ArticuloDTO
+    {
+       
+        public string CodArticulo { get; set; }
+        public string NombreArticulo { get; set; }
+        public int IdMarca { get; set; }
+        public int IdCategoria { get; set; }
+        public decimal Precio { get; set; }
+        public string Descripcion { get; set; }
+
+
+    }
+}
